f13a492a81362471c3f72c937dfe5d1017896ba1
xjxxxj <1690091317@qq.com>
2021-05-05 23:58:15 +0800

    尝试启动StandaloneApplicationClusterEntryPoint类

--- flink-stanalonde-demo/src/main/java/StandaloneApplicationTest.java
+++ flink-stanalonde-demo/src/main/java/StandaloneApplicationTest.java
@@ -1,10 +1,21 @@
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.container.entrypoint.StandaloneApplicationClusterEntryPoint;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 
 public class StandaloneApplicationTest {
 
 
     public static void main(String[] args) {
-        StandaloneApplicationClusterEntryPoint.main(args);
+
+        String[] myArgs = {
+                "--configDir", "E:\\coding\\flink-test\\flink-stanalonde-demo\\conf",
+                "--job-classname", "spendreport.FraudDetectionJob"
+        };
+
+        //StreamExecutionEnvironment fsEnv = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+
+        StandaloneApplicationClusterEntryPoint.main(myArgs);
     }
 
 }
---
+++ flink-stanalonde-demo/src/main/java/TaskManagerRunnerTest.java
@@ -0,0 +1,11 @@
+import org.apache.flink.runtime.taskexecutor.TaskManagerRunner;
+
+public class TaskManagerRunnerTest {
+
+    public static void main(String[] args) throws Exception {
+        String[] myArgs = {
+        };
+        TaskManagerRunner.main(myArgs);
+    }
+
+}
